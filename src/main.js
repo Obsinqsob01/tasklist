72bb86bbb0e79acf35596ac3c092ee8e70c506f7
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-10-03 12:03:41 -0500

    Update main.js by set the firebase credentials

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'                              
 
 Vue.config.productionTip = false
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAoPCV7j7_6FelNucn145FJ1xOKx-C_V30",
+  authDomain: "tasklist-b105a.firebaseapp.com",
+  databaseURL: "https://tasklist-b105a.firebaseio.com",
+  projectId: "tasklist-b105a",
+  storageBucket: "",
+  messagingSenderId: "966784454663"
+})
 
 new Vue({
   router,
